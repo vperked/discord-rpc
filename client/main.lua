b3643f4824fc87ad3028ae98d1e673626c988821
vperked <perked@riseup.net>
2024-06-08 19:15:07 -0400

    Stuff!

---
+++ client/main.lua
@@ -0,0 +1,9 @@
+Citizen.CreateThread(function ()
+    while true do
+        local appid = "1222215777520849057"
+        SetDiscordAppId(appid)
+        SetDiscordRichPresenceAsset("bg")
+        SetDiscordRichPresenceAssetSmallText("Hi")
+    end
+    
+end)
---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'Perk'
+description 'A discord RPC script'
+version '1.0.0'
+
+client_script 'client/main.lua'
