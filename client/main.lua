c5c083251d7137f06b8e8c05d04cc9286ec20ee0
vperked <perked@riseup.net>
2024-06-09 23:23:18 -0400

    Oh ok

--- client/main.lua
+++ client/main.lua
@@ -1,5 +1,4 @@
 local function  main()
-    while true do
     RegisterCommand("start", function (source,args)
     TriggerServerEvent("StartUp")
     end, false)
@@ -9,7 +8,16 @@ local function  main()
     SetDiscordRichPresenceAssetText("Playing  !")
     SetDiscordRichPresenceAction(0, "Play Now!", "fivem://connect/")
     SetDiscordRichPresenceAction(1, "Website", "https://placeholder.lol")
+    while true do
+        Citizen.Wait(200)
     end
 end
 
 Citizen.CreateThread(main)
+
+
+TriggerEvent("chat:addMessage", {
+    color = {255, 255, 255},
+    multiline = true,
+    args = { GetPlayerServerId, "Hello, this is the message that will show in chat" }
+})
